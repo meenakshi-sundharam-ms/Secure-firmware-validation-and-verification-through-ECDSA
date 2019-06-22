e31c25c391305e7c00b3eb984435824f9c6f26d4
meenakshi-sundharam-ms <52074121+meenakshi-sundharam-ms@users.noreply.github.com>
2019-06-22 11:01:52 +0530

    Add files via upload

---
+++ cliexamp.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python3
+import socket
+import collections
+import hashlib
+import random
+import tkinter
+import tkinter.messagebox
+import webbrowser
+import ipaddress
+
+EllipticCurve = collections.namedtuple('EllipticCurve', 'name p a b g n h')
+
+curve = EllipticCurve(
+    'secp256k1',
+    # Field characteristic.
+    p=0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f,
+    # Curve coefficients.
+    a=0,
+    b=7,
+    # Base point.
+    g=(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+       0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8),
+    # Subgroup order.
+    n=0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141,
+    # Subgroup cofactor.
+    h=1,
+)
+
+
+# Modular arithmetic ##########################################################
+
+def inverse_mod(k, p):
+    """Returns the inverse of k modulo p.
+    This function returns the only integer x such that (x * k) % p == 1.
+    k must be non-zero and p must be a prime.
+    """
+    if k == 0:
+        raise ZeroDivisionError('division by zero')
+
+    if k < 0:
+        # k ** -1 = p - (-k) ** -1  (mod p)
+        return p - inverse_mod(-k, p)
+
+    # Extended Euclidean algorithm.
+    s, old_s = 0, 1
+    t, old_t = 1, 0
+    r, old_r = p, k
+
+    while r != 0:
+        quotient = old_r // r
+        old_r, r = r, old_r - quotient * r
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+
+    gcd, x, y = old_r, old_s, old_t
+
+    assert gcd == 1
+    assert (k * x) % p == 1
+
+    return x % p
+
+
+# Functions that work on curve points #########################################
+
+def is_on_curve(point):
+    """Returns True if the given point lies on the elliptic curve."""
+    if point is None:
+        # None represents the point at infinity.
+        return True
+
+    x, y = point
+
+    return (y * y - x * x * x - curve.a * x - curve.b) % curve.p == 0
+
+
+def point_neg(point):
+    """Returns -point."""
+    assert is_on_curve(point)
+
+    if point is None:
+        # -0 = 0
+        return None
+
+    x, y = point
+    result = (x, -y % curve.p)
+
+    assert is_on_curve(result)
+
+    return result
+
+
+def point_add(point1, point2):
+    """Returns the result of point1 + point2 according to the group law."""
+    assert is_on_curve(point1)
+    assert is_on_curve(point2)
+
+    if point1 is None:
+        # 0 + point2 = point2
+        return point2
+    if point2 is None:
+        # point1 + 0 = point1
+        return point1
+
+    x1, y1 = point1
+    x2, y2 = point2
+
+    if x1 == x2 and y1 != y2:
+        # point1 + (-point1) = 0
+        return None
+
+    if x1 == x2:
+        # This is the case point1 == point2.
+        m = (3 * x1 * x1 + curve.a) * inverse_mod(2 * y1, curve.p)
+    else:
+        # This is the case point1 != point2.
+        m = (y1 - y2) * inverse_mod(x1 - x2, curve.p)
+
+    x3 = m * m - x1 - x2
+    y3 = y1 + m * (x3 - x1)
+    result = (x3 % curve.p,
+              -y3 % curve.p)
+
+    assert is_on_curve(result)
+
+    return result
+
+
+def scalar_mult(k, point):
+    """Returns k * point computed using the double and point_add algorithm."""
+    assert is_on_curve(point)
+
+    if k % curve.n == 0 or point is None:
+        return None
+
+    if k < 0:
+        # k * point = -k * (-point)
+        return scalar_mult(-k, point_neg(point))
+
+    result = None
+    addend = point
+
+    while k:
+        if k & 1:
+            # Add.
+            result = point_add(result, addend)
+
+        # Double.
+        addend = point_add(addend, addend)
+
+        k >>= 1
+
+    assert is_on_curve(result)
+
+    return result
+
+
+# Keypair generation and ECDSA ################################################
+
+def make_keypair():
+    """Generates a random private-public key pair."""
+    private_key = random.randrange(1, curve.n)
+    public_key = scalar_mult(private_key, curve.g)
+
+    return private_key, public_key
+
+
+def hash_message(message):
+    """Returns the truncated SHA521 hash of the message."""
+    message_hash = hashlib.sha512(str(message).encode('UTF-8')).digest()
+    e = int.from_bytes(message_hash, 'big')
+
+    # FIPS 180 says that when a hash needs to be truncated, the rightmost bits
+    # should be discarded.
+    z = e >> (e.bit_length() - curve.n.bit_length())
+
+    assert z.bit_length() <= curve.n.bit_length()
+
+    return z
+
+
+def sign_message(private_key, message):
+    z = hash_message(message)
+
+    r = 0
+    s = 0
+
+    while not r or not s:
+        k = random.randrange(1, curve.n)
+        x, y = scalar_mult(k, curve.g)
+
+        r = x % curve.n
+        s = ((z + r * private_key) * inverse_mod(k, curve.n)) % curve.n
+
+    return (r, s)
+
+
+def verify_signature(public_key, message, signature):
+    z = hash_message(message)
+
+    r, s = signature
+
+    w = inverse_mod(s, curve.n)
+    u1 = (z * w) % curve.n
+    u2 = (r * w) % curve.n
+
+    x, y = point_add(scalar_mult(u1, curve.g),
+                     scalar_mult(u2, public_key))
+
+    if (r % curve.n) == (x % curve.n):
+        return 'signature matches'
+    else:
+        return 'invalid signature'
+
+def client_program():
+    private, public = make_keypair()
+    host = '192.168.43.3'
+    port = 5000  
+
+    client_socket = socket.socket()  
+    client_socket.connect((host, port)) 
+
+    data = client_socket.recv(1024).decode()  
+    print('Received from server: ' + data) 
+    signature = sign_message(private, data)
+    print ("Verification status is: ",verify_signature(public,'meena',signature))
+    if (verify_signature(public,'meena',signature) == "signature matches"):
+        #message = input(" Enter passcode ") 
+         top = tkinter.Tk()
+         top.title('client')
+         def getword():
+            message=value.get()
+            client_socket.send(message.encode())  
+            top.destroy()
+            data = client_socket.recv(1024).decode()
+            if(data != 'error'):
+                webbrowser.open(str(data))
+            else:
+                tkinter.messagebox.showinfo("error"+"client","the passcode is not correct")
+         frame = tkinter.Frame(top)    
+
+         L1 = tkinter.Label(frame, text="passcode")
+         L1.pack( side = tkinter.LEFT)
+         value = tkinter.Entry(frame, bd =5)
+         value.pack(side = tkinter.RIGHT)
+         B = tkinter.Button(frame, text ="Submit",command = getword)
+         B.pack(side = tkinter.LEFT)
+         frame.pack()
+         top.mainloop() 
+    else:
+        message = 'error'
+        tkinter.messagebox.showinfo(message+"client","the server is not validated")
+        client_socket.send(message.encode()) 
+
+    client_socket.close()
+
+
+if __name__ == '__main__':
+    client_program()
+
